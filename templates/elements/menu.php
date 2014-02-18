6bbdca01ec258ea3dec4ff74b9057abff8390896
Hung Tran <nguyentieuhau@gmail.com>
2014-02-18 17:30:23 +0700

    Remove J2.5 submenu

--- templates/elements/menu.php
+++ templates/elements/menu.php
@@ -1,13 +1,5 @@
-	if(version_compare(JVERSION,'3','<')){
 		JSubMenuHelper::addEntry(
 			JText::_('##Plural##'),
 			'index.php?option=com_##component##&view=##plural##',
 			($vName == '##plural##')
-		);	
-	} else {
-		JHtmlSidebar::addEntry(
-			JText::_('##Plural##'),
-			'index.php?option=com_##component##&view=##plural##',
-			($vName == '##plural##')
-		);	
-	}
+		);
